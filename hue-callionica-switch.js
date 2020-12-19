42a9770fc935a80e410300a2c4fc5787181bba21
Callionica <16260283+callionica@users.noreply.github.com>
2020-12-19 11:02:45 +0000

    move isn't a click

--- hue-callionica-switch.js
+++ hue-callionica-switch.js
@@ -20,16 +20,25 @@ export class ActionHandler {
         document.addEventListener("mouseup", (e) => this.up_(e));
 
         document.addEventListener("touchstart", (e) => this.down_(e));
-        document.addEventListener("touchend", (e) => { this.up_(e); e.preventDefault(); });
+        document.addEventListener("touchend", (e) => { this.up_(e); });
+        document.addEventListener("touchcancel", (e) => { this.clear(); });
     }
 
     down_(e) {
-        if ((e.buttons !== undefined) && e.buttons !== 1) {
-            return;
+        if ((e.buttons !== undefined)) {
+            if (e.buttons !== 1) {
+                return;
+            }
+            this.clientX = e.clientX;
+            this.clientY = e.clientY;
         }
 
-        if ((e.touches !== undefined) && e.touches.length !== 1) {
-            return;
+        if ((e.touches !== undefined)) {
+            if (e.touches.length !== 1) {
+                return;
+            }
+            this.clientX = e.touches[0].clientX;
+            this.clientY = e.touches[0].clientY;
         }
 
         clearInterval(this.repeater);
@@ -57,20 +66,43 @@ export class ActionHandler {
     }
 
     up_(e) {
-        if ((e.buttons !== undefined) && e.buttons !== 0) {
-            return;
+        let clientX;
+        let clientY;
+
+        if ((e.buttons !== undefined)) {
+            if (e.buttons !== 0) {
+                return;
+            }
+            clientX = e.clientX;
+            clientY = e.clientY;
         }
 
-        if ((e.touches !== undefined) && e.touches.length !== 0) {
-            return;
+        if ((e.touches !== undefined)) {
+            if (e.touches.length !== 0) {
+                return;
+            }
+            clientX = e.touches[0].clientX;
+            clientY = e.touches[0].clientY;
         }
 
         clearInterval(this.repeater);
 
+        console.log(clientX, clientY);
+        console.log(this.clientX, this.clientY);
+
+        const limit = 4;
+        if ((Math.abs(clientX - this.clientX) > limit) || (Math.abs(clientY - this.clientY) > limit)) {
+            return;
+        }
+
         if (this.actionElement !== undefined) {
-            this.up(this.actionElement, this.action);
-            this.actionElement = undefined;
-            this.action = undefined;
+            const actionElement = getActionElement(e.srcElement);
+            if (actionElement === this.actionElement) {
+                e.preventDefault();
+                this.up(this.actionElement, this.action);
+                this.actionElement = undefined;
+                this.action = undefined;
+            }
         }
     }
 
