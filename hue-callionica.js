d135374b892c05706b168b4c8cac021f7beb6c6c
Callionica <16260283+callionica@users.noreply.github.com>
2021-01-20 15:24:24 +0000

    buttonevent not modifiable

--- hue-callionica.js
+++ hue-callionica.js
@@ -193,6 +193,14 @@ export async function setSensorValue(connection, id, value) {
     return put(address, body);
 }
 
+// buttonevent is not modifiable
+// export async function setSensorButtonEvent(connection, id, value) {
+//     const store = "buttonevent";
+//     const address = Address(connection, `sensors/${id}/state`);
+//     const body = `{ "${store}": ${value} }`;
+//     return put(address, body);
+// }
+
 export async function setGroupOn(connection, id, value) {
     const address = Address(connection, `groups/${id}/action`);
     const body = { on: value };
